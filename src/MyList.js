399aaa4cf4356d00990d96856fd408837ac66c86
kimoninanina <nina02929@gmail.com>
2021-05-23 16:54:26 -0500

    data

--- src/MyList.js
+++ src/MyList.js
@@ -2,48 +2,17 @@ import React from 'react';
 import './MyList.css';
 import logo from './logo.jpg';
 
+
+const data = require('./data.json');
+
 class MyList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: {
-        mylist: [
-          {
-            title: 'Futurama',
-            id: 1,
-            img: 'http://cdn1.nflximg.net/webp/7621/3787621.webp',
-          },
-          {
-            title: 'The Interview',
-            id: 2,
-            img: 'http://cdn1.nflximg.net/webp/1381/11971381.webp',
-          },
-          {
-            title: 'Gilmore Girls',
-            id: 3,
-            img: 'http://cdn1.nflximg.net/webp/7451/11317451.webp',
-          },
-        ],
-        recommendations: [
-          {
-            title: 'Family Guy',
-            id: 4,
-            img: 'http://cdn5.nflximg.net/webp/5815/2515815.webp',
-          },
-          {
-            title: 'The Croods',
-            id: 5,
-            img: 'http://cdn3.nflximg.net/webp/2353/3862353.webp',
-          },
-          {
-            title: 'Friends',
-            id: 6,
-            img: 'http://cdn0.nflximg.net/webp/3200/9163200.webp',
-          },
-        ],
-      },
+      data: data,
       showId: '',
     };
+
     this.deleteFun = this.deleteFun.bind(this);
     this.addFun = this.addFun.bind(this);
     this.getCard = this.getCard.bind(this);
